4ec206f2f0c43c97a59dcdac86d618fcd423e8cf
Hamed <ostovari.eng@gmail.com>
2025-08-27 19:14:13 +0200

    Implemented Hero Header / Gender Collection components

--- frontend/src/components/layout/CartDrawer.tsx
+++ frontend/src/components/layout/CartDrawer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { IoClose } from "react-icons/io5";
 import CardContents from "../card/CardContents";
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "../components/layout/pages/home/Hero";
+import GenderCollectionSection from "../components/products/GenderCollectionSection";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Hero />
+      <GenderCollectionSection />
+    </div>
+  );
 };
 
 export default HomePage;
