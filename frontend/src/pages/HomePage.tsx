d2957e988afcf1341b814e4cd46a1cf3cbd9ef6b
Hamed <ostovari.eng@gmail.com>
2025-08-27 20:08:38 +0200

    Implemented NewArrival component/ Implemented horizontal carousel with Dragable feature

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/layout/pages/home/Hero";
 import GenderCollectionSection from "../components/products/GenderCollectionSection";
+import NewArrivals from "../components/products/NewArrivals";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
       <GenderCollectionSection />
+      <NewArrivals />
     </div>
   );
 };
