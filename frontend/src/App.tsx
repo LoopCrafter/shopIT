f0522e26ea3f37735e4696d636fb717b3df18955
Hamed <ostovari.eng@gmail.com>
2025-09-02 19:43:38 +0200

    installed typescript in the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
 "use client";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />}>
-          {" "}
-        </Route>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
       </Routes>
       <Footer />
     </>
