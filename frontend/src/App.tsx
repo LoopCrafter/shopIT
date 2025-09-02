f3845a6c5d3678dbfab50b9c3e91d2cf8e0a0a60
Hamed <ostovari.eng@gmail.com>
2025-09-02 20:05:33 +0200

    Implemented Login and Register UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Footer from "./components/common/Footer";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import { Route, Routes } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
       </Routes>
       <Footer />
     </>
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,14 +1,16 @@
 import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+const RegisterPage = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log("Form Submitted", { email, password });
+    console.log("Form Submitted", { name, email, password });
   };
+
   return (
     <div className="flex">
       <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12">
@@ -22,6 +24,18 @@ const LoginPage = () => {
           <p className="text-center mb-6">
             Enter your username and password to login:
           </p>
+          <div className="mb-4">
+            <label htmlFor="" className="block text-sm font-semibold mb-2">
+              Name
+            </label>
+            <input
+              type="text"
+              className="w-full p-2 border border-gray-200 rounded-lg"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter your Name"
+            />
+          </div>
           <div className="mb-4">
             <label htmlFor="" className="block text-sm font-semibold mb-2">
               Email
@@ -50,19 +64,19 @@ const LoginPage = () => {
             type="submit"
             className="w-full bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transition "
           >
-            Sign In
+            Sign Up
           </button>
           <p className="text-center text-sm mt-6">
-            Don't Have an account?{" "}
-            <Link to="/register" className="text-blue-500">
-              Register
+            Do you Have an account already?{" "}
+            <Link to="/login" className="text-blue-500">
+              Login
             </Link>
           </p>
         </form>
       </div>
       <div className="hidden md:block w-1/2 bg-gray-800">
         <img
-          src="login.webp"
+          src="register.webp"
           alt="login"
           className="h-[750px] w-full object-cover"
         />
@@ -71,4 +85,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
